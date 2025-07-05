2286591af11104d85c2e03ce429a127afe3fb364
serafi1204 <zlakalzn@gmail.com>
2025-07-06 00:48:28 +0900

    1.0.2

--- CatFaceNet/utile.py
+++ CatFaceNet/utile.py
@@ -2,8 +2,6 @@ from .model import getFaseNet
 from importlib.resources import files
 import numpy as np
 import torch
-from torch import nn
-from torchvision import transforms
 import torch.nn.functional as F
 
 profile_default_path = files('CatFaceNet').joinpath('profile.npy')
@@ -11,21 +9,9 @@ profile_default_path = files('CatFaceNet').joinpath('profile.npy')
 class CatDetector():
     def __init__(self, profile_path=profile_default_path):
         self.profile = torch.from_numpy(np.load(profile_path))
-        # Model
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.input_shape = (3, 255, 255)
-        self.model = getFaseNet().to(self.device)
-        self.model.eval()
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
 
-    def __call__(self, cat):
-        cat = self.transform(cat).unsqueeze(0).to(self.device)
-        with torch.no_grad():
-            embedding = self.model(cat)
 
+    def __call__(self, embedding):
         similarity = F.cosine_similarity(embedding, self.profile, dim=1)
         best = int(torch.argmax(similarity).cpu().item())
 
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import CatFaceNet as cfn
 import numpy as np
+import torch
+from torchvision import transforms
 
+# Detector
 whichCat = cfn.CatDetector()
-
+# Model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_shape = (3, 255, 255)
+model = cfn.getFaseNet().to(device)
+model.eval()
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+# main
 cat_img = np.ones((224, 224, 3), dtype=np.uint8)
-cat = whichCat(cat_img)
+cat = transform(cat_img).unsqueeze(0).to(device)
+with torch.no_grad():
+    embedding = model(cat)
+
+cat = whichCat(embedding)
 
 print(cat)
